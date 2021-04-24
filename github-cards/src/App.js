6b7ab25f86466f372cbc816b2766c63e938a0edb
nitin.pa <nitinpandey304@gmail.com>
2021-04-24 18:32:27 +0530

    Improvements

--- github-cards/src/App.js
+++ github-cards/src/App.js
@@ -4,15 +4,34 @@ import React from "react";
 import CardList from "./Components/CardList/CardList";
 import Form from "./Components/Form/Form";
 
+
+const testData = [
+  {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+{name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+{name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+];
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      profiles: testData
+    };
+  }
+
+  addNewProfile = (profileData) => {
+    this.setState(prevState => ({
+      profiles: [...prevState.profiles, profileData]
+    }))
+  }
   render() {
     return (
       <div>
         <div className="header">
           {this.props.title}
         </div>
-        <Form />
-        <CardList />
+        <Form onSubmit = {this.addNewProfile}/>
+        <CardList testData={this.state.profiles}/>
       </div>
     );
   }
--- github-cards/src/Components/Form/Form.js
+++ github-cards/src/Components/Form/Form.js
@@ -1,10 +1,22 @@
 import React from "react"
+import axios from "axios";
 
 class Form extends React.Component {
+    state = {userName: ''}
+    handleSubmit = async (event) => {
+        event.preventDefault();
+        const response = await axios.get(`https://api.github.com/users/${this.state.userName}`);
+        this.props.onSubmit(response.data);
+        this.setState({userName: ''})
+    };
     render() {
         return (
-            <form action = ""> 
-                <input type = "text" placeholder = "Github Username" />
+            <form onSubmit={this.handleSubmit}> 
+                <input type = "text" 
+                placeholder = "Github Username"  
+                value = {this.state.userName}
+                onChange = {event => this.setState({userName: event.target.value})}
+                required/>
                 <button>Add Card</button>
             </form>
         )
